5753659c316b2afb7e8c9c0b5b3f0a57727fea7f
esmaeil1377 <pahang1378@gmail.com>
2020-07-26 06:54:54 +0430

    felix win mood in win activity (not complete)

--- app/src/main/java/com/example/fixitflexjr/WinOrLoseActivity.java
+++ app/src/main/java/com/example/fixitflexjr/WinOrLoseActivity.java
@@ -3,7 +3,10 @@ package com.example.fixitflexjr;
 import android.app.Activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +15,11 @@ import androidx.annotation.Nullable;
 
 public class WinOrLoseActivity extends Activity {
 
+    private Bitmap[] felixWin;
+    private int blockWSize;
+    private int blockHSize;
+    private int screenWidth, screenHeight;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,34 @@ public class WinOrLoseActivity extends Activity {
         setContentView(R.layout.win_or_lose_layout);
         boolean win = Globals.getInstance().wonLastGame;
         int level = Globals.getInstance().getLevel();
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        screenWidth = metrics.widthPixels;
+        screenHeight = metrics.heightPixels;
+        blockWSize = (int) (screenWidth / 6.5);
+        blockWSize = (blockWSize / 5) * 5;
+
+        blockHSize = (int) (screenHeight / 11);
+        blockHSize = (blockHSize / 5) * 5;
+        int felixWSize = (int) (blockWSize);
+        felixWSize = (felixWSize / 5) * 5;
+
+        int felixHSize = (int) (blockHSize * 0.8);
+        felixHSize = (felixHSize / 5) * 5;
+
+        felixWin = new Bitmap[6];
+        felixWin[0] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.felixwin0), felixWSize, felixHSize, false);
+        felixWin[1] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.felixwin1), felixWSize, felixHSize, false);
+        felixWin[2] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.felixwin2), felixWSize, felixHSize, false);
+        felixWin[3] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.felixwin3), felixWSize, felixHSize, false);
+        felixWin[4] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.felixwin4), felixWSize, felixHSize, false);
+        felixWin[5] = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                getResources(), R.drawable.felixwin5), felixWSize, felixHSize, false);
         if (win){
             Globals.getInstance().incrementLevel();
         }
@@ -27,6 +63,7 @@ public class WinOrLoseActivity extends Activity {
         String s;
         if (win) {
             s = "you win";
+
         } else {
             s = "you lose";
         }
