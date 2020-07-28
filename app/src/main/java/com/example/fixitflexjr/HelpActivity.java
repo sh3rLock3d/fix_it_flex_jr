d9103803de99232ddb95b1cd9286e89324da449d
esmaeil1377 <pahang1378@gmail.com>
2020-07-28 06:25:29 +0430

    complete felix win mood in win activity and complete help activity

--- app/src/main/java/com/example/fixitflexjr/HelpActivity.java
+++ app/src/main/java/com/example/fixitflexjr/HelpActivity.java
@@ -2,6 +2,7 @@ package com.example.fixitflexjr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelpActivity extends Activity {
     @Override
@@ -9,6 +10,17 @@ public class HelpActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help_layout);
         MainActivity.getGameSong().start();
+        TextView textView = findViewById(R.id.textView2);
+        String text;
+        text = "Hi Guys \n" +
+                "Welcome to fix it flex jr game \n" +
+                "In this game you have to play against the saboteur Ralph and repair his sabotage" +
+                " so that the inhabitants of the city will be happy and proud of you.\n" +
+                "How to play now?\n" +
+                "To move in the direction you want to pull your finger in the desired direction," +
+                " Also, to repair the windows of the building, go to the desired window and click on it several times.\n" +
+                "Finally, you should always be careful that the bricks do not hit you, and if you fix all the windows, you will go to the next level.";
+        textView.setText(text);
     }
 
     @Override
--- app/src/main/java/com/example/fixitflexjr/WinOrLoseActivity.java
+++ app/src/main/java/com/example/fixitflexjr/WinOrLoseActivity.java
@@ -5,10 +5,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -63,7 +66,11 @@ public class WinOrLoseActivity extends Activity {
         String s;
         if (win) {
             s = "you win";
-
+            ImageView tv1;
+            tv1= (ImageView) findViewById(R.id.imageView);
+            Bitmap bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(
+                    getResources(), R.drawable.felixwin), felixWSize, felixHSize, false);
+            tv1.setImageBitmap(bitmap);
         } else {
             s = "you lose";
         }
