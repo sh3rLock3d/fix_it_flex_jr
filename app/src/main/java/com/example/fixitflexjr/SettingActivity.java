2784543bf59c0a88e5bae5eb803f0652a228b987
esmaeil1377 <pahang1378@gmail.com>
2020-08-03 03:09:20 +0430

    setting screen not complete

--- app/src/main/java/com/example/fixitflexjr/SettingActivity.java
+++ app/src/main/java/com/example/fixitflexjr/SettingActivity.java
@@ -8,34 +8,41 @@ import android.widget.Switch;
 
 
 public class SettingActivity extends Activity {
+    private boolean checkMusic = true;
+    private boolean checkSounds = true;
+    private boolean checkNotifications = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_layout);
         MainActivity.getGameSong().start();
         Switch musicSwitch = findViewById(R.id.switch_music);
+        musicSwitch.setChecked(checkMusic);
         Switch soundsSwitch = findViewById(R.id.switch_sounds);
+        soundsSwitch.setChecked(checkSounds);
         Switch notificationsSwitch = findViewById(R.id.switch_notifications);
+        notificationsSwitch.setChecked(checkNotifications);
         musicSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                checkMusic = isChecked;
                 if (!isChecked) {
                     MainActivity.getGameSong().stop();
                 } else {
-                    MainActivity.getGameSong().start();
+                    MainActivity.getGameSong().reset();
                 }
             }
         });
         soundsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+                checkSounds = isChecked;
             }
         });
         notificationsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+                checkNotifications = isChecked;
             }
         });
     }
